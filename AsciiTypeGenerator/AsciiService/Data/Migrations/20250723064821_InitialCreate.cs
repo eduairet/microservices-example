84059355d0510dc9f994fdd0f9c448547359f52b
Eduardo Aire <hola@eduairet.com>
2025-07-23 00:51:12 -0600

    Add initial EF Core migration and update Glyph entity
    Added initial Entity Framework Core migration files to set up database schema for Matrices, Users, Alphabets, Artworks, and Glyphs. Updated Glyph entity to store Drawing as a serialized string in the database, replacing the previous use of MatricesEnum for MatrixId and implementing serialization/deserialization logic for Drawing.

---
+++ AsciiTypeGenerator/AsciiService/Data/Migrations/20250723064821_InitialCreate.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AsciiService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Matrices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Depth = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matrices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", maxLength: 2147483647, nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", maxLength: 2147483647, nullable: false),
+                    UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Alphabets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AuthorId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alphabets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Alphabets_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Artworks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AuthorId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artworks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Artworks_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Glyphs",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "text", maxLength: 2147483647, nullable: false),
+                    Unicode = table.Column<int>(type: "integer", nullable: false),
+                    MatrixId = table.Column<int>(type: "integer", nullable: false),
+                    AlphabetId = table.Column<int>(type: "integer", nullable: false),
+                    DrawingSerialized = table.Column<string>(type: "text", nullable: false),
+                    ArtworkId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Glyphs", x => x.Name);
+                    table.ForeignKey(
+                        name: "FK_Glyphs_Alphabets_AlphabetId",
+                        column: x => x.AlphabetId,
+                        principalTable: "Alphabets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Glyphs_Artworks_ArtworkId",
+                        column: x => x.ArtworkId,
+                        principalTable: "Artworks",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Glyphs_Matrices_MatrixId",
+                        column: x => x.MatrixId,
+                        principalTable: "Matrices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alphabets_AuthorId",
+                table: "Alphabets",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alphabets_Title",
+                table: "Alphabets",
+                column: "Title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artworks_AuthorId",
+                table: "Artworks",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artworks_Title",
+                table: "Artworks",
+                column: "Title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Glyphs_AlphabetId",
+                table: "Glyphs",
+                column: "AlphabetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Glyphs_ArtworkId",
+                table: "Glyphs",
+                column: "ArtworkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Glyphs_MatrixId",
+                table: "Glyphs",
+                column: "MatrixId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matrices_Depth",
+                table: "Matrices",
+                column: "Depth",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matrices_Name",
+                table: "Matrices",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Glyphs");
+
+            migrationBuilder.DropTable(
+                name: "Alphabets");
+
+            migrationBuilder.DropTable(
+                name: "Artworks");
+
+            migrationBuilder.DropTable(
+                name: "Matrices");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- AsciiTypeGenerator/AsciiService/Entities/Glyph.cs
+++ AsciiTypeGenerator/AsciiService/Entities/Glyph.cs
@@ -7,9 +7,27 @@ public class Glyph
 {
     [Key] [MaxLength(int.MaxValue)] public string Name { get; set; } = string.Empty;
     [Required] public int Unicode { get; set; }
-    [Required] public List<List<char>> Drawing { get; set; } = [];
-    [Required] public MatricesEnum MatrixId { get; set; }
+    [Required] public int MatrixId { get; set; }
     [ForeignKey("MatrixId")] public Matrix Matrix { get; set; } = new();
     [Required] public int AlphabetId { get; set; }
     [ForeignKey("AlphabetId")] public Alphabet Alphabet { get; set; }
+
+    [Required]
+    public string DrawingSerialized
+    {
+        get => SerializeDrawing(Drawing);
+        set => Drawing = DeserializeDrawing(value);
+    }
+
+    [NotMapped] private List<List<char>> Drawing { get; set; } = [];
+
+    private static string SerializeDrawing(List<List<char>> drawing)
+    {
+        return System.Text.Json.JsonSerializer.Serialize(drawing);
+    }
+
+    private static List<List<char>> DeserializeDrawing(string serialized)
+    {
+        return System.Text.Json.JsonSerializer.Deserialize<List<List<char>>>(serialized) ?? [];
+    }
 }
