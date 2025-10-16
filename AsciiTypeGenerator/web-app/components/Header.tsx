b604b67480a51306ffe6d4cf0cb556a3f8721c3c
Eduardo Aire <hola@eduairet.com>
2025-10-16 00:04:14 -0600

    Remove unused import of Atg in Header component

--- AsciiTypeGenerator/web-app/components/Header.tsx
+++ AsciiTypeGenerator/web-app/components/Header.tsx
@@ -1,6 +1,5 @@
 import { type FC } from 'react';
 import NavLinkHome from './NavLinkHome';
-import Atg from './Atg';
 
 const Header: FC = () => (
   <header className="mx-auto max-w-5xl p-10 flex items-center w-full justify-between">
